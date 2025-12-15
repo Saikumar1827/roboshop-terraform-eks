08fd44687a784981bbe61605de28dda51b06c809
sai goud <saikumarmogulla40@gmail.com>
2025-12-15 18:35:06 +0530

    k8

---
+++ 70-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "domain_name" {
+    default = "saidaws86s.fun"
+}
+
+variable "zone_id" {
+    default = "Z0506429205Z4ZA4IXHNC"
+}
---
+++ 90-eks/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z0506429205Z4ZA4IXHNC"
+}
+
+variable "domain_name" {
+    default = "saidaws86s.fun"
+}
