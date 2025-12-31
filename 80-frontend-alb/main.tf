51dab0d2acefc437eb558abea4f97f6991224670
sai goud <saikumarmogulla40@gmail.com>
2026-01-01 01:54:15 +0530

    jenkins

--- 80-frontend-alb/main.tf
+++ 80-frontend-alb/main.tf
@@ -36,7 +36,7 @@ resource "aws_lb_listener" "ingress_alb" {
 
 resource "aws_route53_record" "ingress_alb" {
   zone_id = var.zone_id
-  name    = "*.${var.domain_name}" # *.daws86s.fun
+  name    = "*.${var.domain_name}" # *.saidaws86s.fun
   type    = "A"
   allow_overwrite = true
 
@@ -45,5 +45,41 @@ resource "aws_route53_record" "ingress_alb" {
     name                   = aws_lb.ingress_alb.dns_name
     zone_id                = aws_lb.ingress_alb.zone_id
     evaluate_target_health = true
+  }
+}
+
+resource "aws_lb_target_group" "frontend" {
+  name     = "${local.common_name_suffix}-frontend"
+  port     = 8080
+  protocol = "HTTP"
+  target_type = "ip"
+  vpc_id   = local.vpc_id
+  deregistration_delay = 60 # waiting period before deleting the instance
+
+  health_check {
+    healthy_threshold = 2
+    interval = 10
+    matcher = "200-299"
+    path = "/"
+    port = 8080
+    protocol = "HTTP"
+    timeout = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "frontend" {
+  listener_arn = aws_lb_listener.ingress_alb.arn
+  priority     = 10
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.frontend.arn
+  }
+
+  condition {
+    host_header {
+      values = ["${var.environment}.${var.domain_name}"] # dev.saidaws86s.fun
+    }
   }
 }
